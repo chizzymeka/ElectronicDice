205e190c35e6694b1091b1885070e9db328316e7
Chizzy Meka <chizzymeka@yahoo.co.uk>
2019-05-05 19:33:32 +0100

    RandomNumberGeneratorBean implementation.

--- src/controller/ControllerServlet.java
+++ src/controller/ControllerServlet.java
@@ -6,10 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.HashMap;
+
+import model.RandomNumberGeneratorBean;
 
 @WebServlet(name = "ControllerServlet", loadOnStartup = 1, urlPatterns={"/generatorRandomNumber"})
 public class ControllerServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
@@ -31,22 +34,22 @@ public class ControllerServlet extends HttpServlet {
             String description = request.getParameter("description");
             String sendEmailCopyToSelf = request.getParameter("sendEmailCopyToSelf");
 
-            System.out.println("numberCount: " + numberCount);
-            System.out.println("lowerRange: " + lowerRange);
-            System.out.println("higherRange: " + higherRange);
-            System.out.println("numberOfFormatColumns: " + numberOfFormatColumns);
-            System.out.println("uniqueness: " + uniqueness);
-            System.out.println("trail: " + trail);
-            System.out.println("emailSubject: " + emailSubject);
-            System.out.println("emailSubemailAddressesject: " + emailAddresses);
-            System.out.println("description: " + description);
-            System.out.println("sendEmailCopyToSelf: " + sendEmailCopyToSelf);
+            RandomNumberGeneratorBean randomNumberGeneratorBean = new RandomNumberGeneratorBean();
+            randomNumberGeneratorBean.setNumberCount(numberCount);
+            randomNumberGeneratorBean.setLowerRange(lowerRange);
+            randomNumberGeneratorBean.setHigherRange(higherRange);
+            randomNumberGeneratorBean.setNumberOfFormatColumns(numberOfFormatColumns);
+            randomNumberGeneratorBean.setUniqueness(uniqueness);
+            randomNumberGeneratorBean.setTrail(trail);
+            randomNumberGeneratorBean.setEmailSubject(emailSubject);
+            randomNumberGeneratorBean.setEmailAddresses(emailAddresses);
+            randomNumberGeneratorBean.setDescription(description);
+            randomNumberGeneratorBean.setSendEmailCopyToSelf(sendEmailCopyToSelf);
 
             userPath = "/index";
         }
 
         String url = userPath + ".jsp";
-        System.out.println("url: " + url);
 
         try {
             request.getRequestDispatcher(url).forward(request, response);
